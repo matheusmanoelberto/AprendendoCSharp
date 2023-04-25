a0f99777619777d95d4bde0ce7c59ff9efd1845c
Matheus Manoel <matheusmanoel51@gmail.com>
2023-04-24 23:22:26 -0300

    atualizacao
    atualizacao24/04/2023

---
+++ p11-CalculaPoupanca/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando o projeto 11 - Calcula Poupança");
+
+        double investimento = 1000;
+            //variavel - condição - computador  
+        for(int mes = 1; mes <= 12; mes = mes + 1)
+        {
+            investimento *= 1.005;
+            Console.WriteLine("No mês " + mes + "você tem R$ " + investimento);
+        }
+        
+        Console.WriteLine("Para finalizar a aplicação precione ENTER....");
+        Console.ReadLine();
+    }
+}
